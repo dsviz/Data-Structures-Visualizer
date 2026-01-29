929858433d4decff2b285222090016f3a0a08026
Shubham Kumar Sharma <shubhamsharma86900@gmail.com>
2026-01-29 11:10:35 +0530

    optimized layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,32 +15,40 @@ import Terms from '../pages/Terms'
 import Privacy from '../pages/Privacy'
 import Login from '../pages/Login'
 import Signup from '../pages/Signup'
+import { useLayout } from '../context/LayoutContext'
+
+function AppContent() {
+  const { isNavbarVisible } = useLayout();
+  return (
+    <div className="flex flex-col h-screen">
+      {isNavbarVisible && <Navbar />}
+      <main className="flex-1 overflow-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/arrays" element={<Arrays />} />
+          <Route path="/graphs" element={<Graphs />} />
+          <Route path="/linked-list" element={<LinkedList />} />
+          <Route path="/queue" element={<Queue />} />
+          <Route path="/sorting" element={<Sorting />} />
+          <Route path="/stack" element={<Stack />} />
+          <Route path="/trees" element={<Trees />} />
+          <Route path="/recursion" element={<Recursion />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col h-screen">
-        <Navbar />
-        <main className="flex-1 overflow-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/arrays" element={<Arrays />} />
-            <Route path="/graphs" element={<Graphs />} />
-            <Route path="/linked-list" element={<LinkedList />} />
-            <Route path="/queue" element={<Queue />} />
-            <Route path="/sorting" element={<Sorting />} />
-            <Route path="/stack" element={<Stack />} />
-            <Route path="/trees" element={<Trees />} />
-            <Route path="/recursion" element={<Recursion />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </main>
-      </div>
+      <AppContent />
     </Router>
   )
 }
--- src/context/LayoutContext.tsx
+++ src/context/LayoutContext.tsx
@@ -4,17 +4,20 @@ interface LayoutContextType {
     isSidebarOpen: boolean;
     toggleSidebar: () => void;
     setIsSidebarOpen: (isOpen: boolean) => void;
+    isNavbarVisible: boolean;
+    setIsNavbarVisible: (isVisible: boolean) => void;
 }
 
 const LayoutContext = createContext<LayoutContextType | undefined>(undefined);
 
 export const LayoutProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isNavbarVisible, setIsNavbarVisible] = useState(true);
 
     const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
 
     return (
-        <LayoutContext.Provider value={{ isSidebarOpen, toggleSidebar, setIsSidebarOpen }}>
+        <LayoutContext.Provider value={{ isSidebarOpen, toggleSidebar, setIsSidebarOpen, isNavbarVisible, setIsNavbarVisible }}>
             {children}
         </LayoutContext.Provider>
     );
