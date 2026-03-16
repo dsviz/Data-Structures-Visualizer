bd985a86ed84aec90daa3fae68d290d7a35c010a
Shubham Kumar Sharma <shubhamsharma86900@gmail.com>
2026-03-16 19:07:36 +0530

    removed unused files

--- tmp_print_lines.py
+++
@@ -1,8 +0,0 @@
-import sys
-path = sys.argv[1]
-start = int(sys.argv[2])
-end = int(sys.argv[3])
-with open(path, 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i in range(start-1, min(end, len(lines))):
-    print(f"{i+1:4d}: {lines[i].rstrip()}")
